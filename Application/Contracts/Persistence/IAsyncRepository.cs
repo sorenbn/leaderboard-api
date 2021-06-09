fb090eb51caf22d9b2137f62dddb054cc33243fd
Sorne Bachse <spankey103@gmail.com>
2021-06-09 15:45:51 +0200

    Setup rest of the project structure

---
+++ Application/Contracts/Persistence/IAsyncRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.Persistence
+{
+    public interface IAsyncRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(Guid id);
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task<T> CreateAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
