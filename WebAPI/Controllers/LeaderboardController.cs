87a2ea74ef5f866bfbcf2ddfd743fc7129c2a5cd
Søren Bach Nielsen <sbn@commentor.dk>
2021-06-11 10:26:08 +0200

    Test

--- WebAPI/Controllers/LeaderboardController.cs
+++ WebAPI/Controllers/LeaderboardController.cs
@@ -5,7 +5,6 @@ using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace WebAPI.Controllers
